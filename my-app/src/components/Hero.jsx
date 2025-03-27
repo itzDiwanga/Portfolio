f37ff1bf930904d29b010d09893d34464dcebfa0
Diwanga Munasinghe <diwangapawani405@gmail.com>
2025-03-27 16:45:57 +0530

    add a profile photo

--- my-app/src/components/Hero.jsx
+++ my-app/src/components/Hero.jsx
@@ -10,14 +10,9 @@ const Hero = () => {
           <p>
             I am currently pursuing my undergraduate studies at the Informatics Institute of Technology (IIT), 
             affiliated with the University of Westminster, majoring in BSc (Hons) with a specialization in 
-            Computer Science, with an anticipated graduation in 2027. I am characterized by my unwavering commitment 
-            to achieving goals and my passion for problem-solving. I approach challenges with confidence, consistently 
-            seeking innovative solutions and fresh perspectives.
+            Computer Science, with an anticipated graduation in 2027
           </p>
-          <p>
-            My career aspirations lie in UI/UX design, frontend development, and backend development, where I can 
-            merge analytical thinking with creativity to build intuitive, seamless, and impactful digital experiences.
-          </p>
+
           <div className="hero-buttons">
             <a href="#contact" className="btn">Contact Me</a>
             <a href="#projects" className="btn btn-outline">View My Work</a>
